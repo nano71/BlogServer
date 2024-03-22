e33a894511348940767df450d5003c6875a81d22
nano71 <80870408+nano71@users.noreply.github.com>
2024-03-22 22:22:19 +0800

    add guestbook.go

--- setup.sql
+++ setup.sql
@@ -34,11 +34,13 @@ create table if not exists tag
 
 create table if not exists guestbook
 (
-    id       int auto_increment primary key,
-    nickname varchar(255) not null,
-    content  text         not null,
-    url      varchar(255)
-
+    id          int auto_increment
+        primary key,
+    nickname    varchar(255)                       not null,
+    face        varchar(128)                       not null,
+    content     text                               not null,
+    url         varchar(255)                       null,
+    create_time datetime default CURRENT_TIMESTAMP not null,
+    ip          varchar(15)                        not null comment 'ip地址'
 )
-    comment "留言板表"
-    charset = utf8mb4
+    comment '留言板表' charset = utf8mb4;
